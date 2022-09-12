10ad421ec92e14511c8db0894e5eb9d31ec08e5e
bbasanta238 <bbasanta238@gmail.com>
2022-09-12 13:53:41 +0545

    ruby operators

---
+++ 4_operators.rb
@@ -0,0 +1,21 @@
+#  + addition
+#  - subtraction
+#  * multiplication
+#  / division
+#  % modulus
+#  ** exponential
+#  == checking operator
+#  != not equal to
+#  > greater
+#  < less
+#  >= greater and equal
+#  <= less and equal
+#  <=>  returns 0 if(a==b) , returns 1 if(a>b) returns -1 if(a<b)
+#  === (1...10) === 5 returns true
+#  += c+=a ==> c = c+a
+#  &  binary and
+# | binary operator
+# && loacl and
+# || logical or
+# .. 1..10 => 1,2,3,....,10
+# ... 1...10 => 1,2,3,...9
