d8c48d166b57b828fe11ab83e38d56e7b65a5348
bbasanta238 <bbasanta238@gmail.com>
2022-09-14 11:58:23 +0545

    split join

---
+++ 10_array_slicing.rb
@@ -0,0 +1,28 @@
+$a = [[1,2,3],[4,5,6],[7,8,9],["hari","shyam"]]
+$b = "I am a magician"
+puts $a.length
+for i in 0...$a.length
+    @j = 0
+    for j in 0..2
+    puts($a[i][j])
+    end
+end
+
+def showEachItem(array)
+    array.each do |a|
+        puts a
+    end
+end
+
+def sliceArray(array)
+    puts(array.slice(0..2))
+end
+
+def splitArray(temp)
+    puts(temp.split(" "))
+    puts((temp.split(" ")).join("a"))
+end
+
+showEachItem($a)
+sliceArray($a)
+splitArray($b)
